1039695d36e31874facd5e2fb8c528157ae0845d
Edward Hervey <bilboed@bilboed.com>
2004-05-10 16:00:25 +0000

    updated autogen.sh to new common/ files
    Original commit message from CVS:
    updated autogen.sh to new common/ files

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 # Run this to generate all the initial makefiles, etc.
 
 DIE=0
@@ -14,62 +14,83 @@ then
 fi
 . common/gnonlin-autogen.sh
 
+CONFIGURE_DEF_OPT=''
+
 autogen_options $@
 
-echo "+ checking for build tools"
-version_check "autoconf" "ftp://ftp.gnu.org/pub/gnu/autoconf/" 2 52 || DIE=1
-version_check "automake" "ftp://ftp.gnu.org/pub/gnu/automake/" 1 6 || DIE=1
-version_check "libtoolize" "ftp://ftp.gnu.org/pub/gnu/libtool/" 1 5 0 || DIE=1
-version_check "pkg-config" "http://www.freedesktop.org/software/pkgconfig" 0 8 0 || DIE=1
+echo -n "+ check for build tools"
+if test ! -z "$NOCHECK"; then echo ": skipped version checks"; else  echo; fi
+version_check "autoconf" "$AUTOCONF autoconf autoconf-2.54 autoconf-2.53 autoconf-2.52" \
+              "ftp://ftp.gnu.org/pub/gnu/autoconf/" 2 52 || DIE=1
+version_check "automake" "$AUTOMAKE automake automake-1.7 automake17 automake-1.6" \
+              "ftp://ftp.gnu.org/pub/gnu/automake/" 1 6 || DIE=1
+#version_check "autopoint" "autopoint" \
+#              "ftp://ftp.gnu.org/pub/gnu/gettext/" 0 11 5 || DIE=1
+version_check "libtoolize" "libtoolize" \
+              "ftp://ftp.gnu.org/pub/gnu/libtool/" 1 5 0 || DIE=1
+version_check "pkg-config" "" \
+              "http://www.freedesktop.org/software/pkgconfig" 0 8 0 || DIE=1
 
-autoconf_2.52d_check || DIE=1
+die_check $DIE
 
-if test "$DIE" -eq 1; then exit 1; fi
+autoconf_2_52d_check || DIE=1
+aclocal_check || DIE=1
+autoheader_check || DIE=1
 
-# CONFIGURE_OPT='--enable-maintainer-mode --enable-plugin-builddir'
-
-CONFIGURE_OPT=""
+die_check $DIE
 
 # if no arguments specified then this will be printed
-
 if test -z "$*"; then
-	echo "+ Checking for autogen.sh options"
-	echo "  This autogen script will automatically run ./configure as:"
-        echo "  ./configure $CONFIGURE_OPT"
-        echo "  To pass any additional options, please specify them on the $0"
-        echo "  command line."
+  echo "+ checking for autogen.sh options"
+  echo "  This autogen script will automatically run ./configure as:"
+  echo "  ./configure $CONFIGURE_DEF_OPT"
+  echo "  To pass any additional options, please specify them on the $0"
+  echo "  command line."
 fi
 
 toplevel_check $srcfile
 
-tool_run "aclocal" "-I common/m4 $ACLOCAL_FLAGS"
+# autopoint
+#    older autopoint (< 0.12) has a tendency to complain about mkinstalldirs
+#if test -x mkinstalldirs; then rm mkinstalldirs; fi
+#    first remove patch if necessary, then run autopoint, then reapply
+#if test -f po/Makefile.in.in;
+#then
+#  patch -p0 -R < common/gettext.patch
+#fi
+#tool_run "$autopoint --force"
+#patch -p0 < common/gettext.patch
 
-# FIXME : why does libtoolize keep complaining about aclocal ?
-# echo "+ not running libtoolize until libtool fix has flown downstream"
-tool_run "libtoolize" "--copy --force"
-tool_run "autoheader"
+# aclocal
+if test -f acinclude.m4; then rm acinclude.m4; fi
+tool_run "$aclocal" "-I common/m4 $ACLOCAL_FLAGS"
+
+tool_run "$libtoolize" "--copy --force"
+tool_run "$autoheader"
 
 # touch the stamp-h.in build stamp so we don't re-run autoheader in maintainer mode -- wingo
 echo timestamp > stamp-h.in 2> /dev/null
-tool_run "autoconf"
-tool_run "automake" "-a -c"
+
+tool_run "$autoconf"
+debug "automake: $automake"
+tool_run "$automake" "--add-missing --copy"
 
 test -n "$NOCONFIGURE" && {
-    echo "skipping configure stage for package $package, as requested."
-    echo "autogen.sh done."
-    exit 0
+  echo "skipping configure stage for package $package, as requested."
+  echo "autogen.sh done."
+  exit 0
 }
 
 echo "+ running configure ... "
-echo "./configure default flags: $CONFIGURE_OPT"
-echo "using: $CONFIGURE_OPT $@"
+test ! -z "$CONFIGURE_DEF_OPT" && echo "  ./configure default flags: $CONFIGURE_DEF_OPT"
+test ! -z "$CONFIGURE_EXT_OPT" && echo "  ./configure external flags: $CONFIGURE_EXT_OPT"
 echo
 
-./configure $CONFIGURE_OPT "$@" || {
-	echo
-	echo "configure failed"
-	exit 1
+echo ./configure $CONFIGURE_DEF_OPT $CONFIGURE_EXT_OPT
+./configure $CONFIGURE_DEF_OPT $CONFIGURE_EXT_OPT || {
+        echo "  configure failed"
+        exit 1
 }
 
-echo 
 echo "Now type 'make' to compile $package."
+
