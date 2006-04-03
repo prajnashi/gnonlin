ea131879e57adbf8283cb627b05f73929e042820
Thomas Vander Stichele <thomas@apestaart.org>
2006-04-03 08:49:17 +0000

    clean up like the other modules
    Original commit message from CVS:
    
    * configure.ac:
    * gnl/Makefile.am:
    * gnl/gnl.c:
    clean up like the other modules

--- gnl/gnl.c
+++ gnl/gnl.c
@@ -56,4 +56,4 @@ GST_PLUGIN_DEFINE (GST_VERSION_MAJOR,
 		   GST_VERSION_MINOR,
 		   "gnonlin",
 		   "Standard elements for non-linear multimedia editing",
-		   plugin_init, VERSION, "LGPL", GST_PACKAGE, GST_ORIGIN);
+		   plugin_init, VERSION, "LGPL", GST_PACKAGE_NAME, GST_PACKAGE_ORIGIN);
