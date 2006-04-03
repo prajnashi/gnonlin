93f5bceda2a96af3d157d238197b0aa1ffd1815a
Edward Hervey <bilboed@bilboed.com>
2006-04-03 13:32:23 +0000

    configure.ac: New pre-release (0.10.0.10).
    Original commit message from CVS:
    * configure.ac:
    New pre-release (0.10.0.10).
    Required version of gstreamer core/-base is 0.10.4.
    * tests/check/simple.c: (gnonlin_suite):
    Only run the tests with core > 0.10.4.0

--- tests/check/simple.c
+++ tests/check/simple.c
@@ -580,14 +580,19 @@ gnonlin_suite (void)
 {
   Suite *s = suite_create ("gnonlin");
   TCase *tc_chain = tcase_create ("general");
+  guint major, minor, micro, nano;
 
   suite_add_tcase (s, tc_chain);
-  tcase_add_test (tc_chain, test_one_after_other);
-  tcase_add_test (tc_chain, test_one_under_another);
 
-  /* Uncomment and fix this after the release 2006-03-29 */
-  /*   tcase_add_test (tc_chain, test_one_above_another); */
-
+  /* Only add the following test for core > 0.10.4 */
+  gst_version (&major, &minor, &micro, &nano);
+  if ((micro > 4) || (micro == 4 && nano > 0)) {
+    tcase_add_test (tc_chain, test_one_after_other);
+    tcase_add_test (tc_chain, test_one_under_another);
+    
+    /* Uncomment and fix this after the release 2006-03-29 */
+    /*   tcase_add_test (tc_chain, test_one_above_another); */
+  }
   return s;
 }
 
