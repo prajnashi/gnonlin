f31b44418b8da9b4983d0e41414bf31866dd1b9f
Edward Hervey <bilboed@bilboed.com>
2009-04-01 12:21:43 +0200

    Android: Makefiles to build gnonlin

---
+++ Android.mk
@@ -0,0 +1,7 @@
+LOCAL_PATH := $(call my-dir)
+
+GNONLIN_TOP := $(LOCAL_PATH)
+
+include $(CLEAR_VARS)
+
+include $(GNONLIN_TOP)/gnl/Android.mk
---
+++ gnl/Android.mk
@@ -0,0 +1,43 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+GST_MAJORMINOR:= 0.10
+
+LOCAL_SRC_FILES:= \
+	gnl.c			\
+	gnlobject.c		\
+	gnlcomposition.c	\
+	gnloperation.c		\
+	gnlsource.c		\
+	gnlfilesource.c
+
+
+LOCAL_SHARED_LIBRARIES := 	\
+	libgstreamer-0.10	\
+	libglib-2.0		\
+	libgthread-2.0		\
+	libgmodule-2.0		\
+	libgobject-2.0
+
+LOCAL_MODULE:= libgnl
+
+LOCAL_C_INCLUDES := \
+	$(LOCAL_PATH)   \
+	external/gstreamer	   \
+	external/gstreamer/android	  \
+	external/gstreamer/gst	\
+	external/gstreamer/gst/android	\
+	external/gstreamer/libs \
+	external/glib   \
+	external/glib/android   \
+	external/glib/glib   \
+	external/glib/gmodule   \
+	external/glib/gobject  \
+	external/glib/gthread
+
+
+LOCAL_CFLAGS := \
+	-DHAVE_CONFIG_H			
+
+include $(BUILD_PLUGIN_LIBRARY)
